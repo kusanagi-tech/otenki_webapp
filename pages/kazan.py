e3396db48007583a5e267ac056d1a8bca92a974c
kusanagi <65207089+kusanagi-tech@users.noreply.github.com>
2024-05-30 04:54:46 +0900

    new function added

--- pages/kazan.py
+++ pages/kazan.py
@@ -1,15 +1,22 @@
 import streamlit as st
 import feedparser
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 
 URL = 'https://www.data.jma.go.jp/developer/xml/feed/eqvol_l.xml'
 d_atom = feedparser.parse(URL)
 count = len(d_atom['entries'])
+
 mydata = []
+myurl = []
 
 for i in range(count):
   base = d_atom['entries'][i]
   mydata.append(base['summary']+base['updated'])
+  myurl.append(base['links'][0]['href'])
+
+data = zip(mydata,myurl)
 
 kazan = {
   "最新10件": '',
@@ -18,22 +25,44 @@ kazan = {
   "口永良部島":"くちのえらぶじま",
   "諏訪之瀬島":"すわのせじま",
   "浅間山":"あさまやま",
-  "阿蘇山":"あそさん"
+  "阿蘇山":"あそさん",
+  "震源・震度情報": ""
 }
 
 st.header("🌋地震火山情報XML",anchor='section1',divider='rainbow')
-mountain = st.radio("**火山を選択**",kazan,index = 0 )
+mountain = st.selectbox("**火山を選択**",kazan,index = 0 )
+
+def linkurl(x):
+  r = requests.get(x)
+  soup = BeautifulSoup(r.text.encode(r.encoding),'lxml-xml')
+  description = soup.find("""jmx_eb:Coordinate""").get("description")
+  m = list(filter(None, soup.get_text().splitlines()))
+  body = [m[0],m[13],m[16],description,m[18],"M"+m[19],m[-4]]
+  return st.table(body)
+  #return st.table(m) #全データー出力
+
+def button(keyword):
+  keyword = keyword.reset_index(drop=True)
+  if st.button(":blue[**詳細情報**]",key=1):
+    for i in range(0,10) :
+      linkurl(keyword.loc[i,'URL'])
+      st.write(keyword.loc[i,'URL'])
+    st.markdown("[go to Top](#section1)")
 
 meisyou = f'{mountain} : {kazan[mountain]}'
-df = pd.DataFrame(data = mydata,columns=[meisyou])
+df = pd.DataFrame(data = data,columns=[meisyou,"URL"])
 keyword = df[df[meisyou].str.contains(mountain)]
 
 if mountain == "最新10件" :
-  st.table(df.head(10))
+  st.table(df[meisyou].head(10))
+elif mountain == "震源・震度情報" : 
+  button(keyword)
+  st.table(keyword[meisyou].head(10)) 
 else :
-  st.table(keyword.head(10))
+  st.table(keyword[meisyou].head(10))
 
 st.write("""出典：気象庁 https://xml.kishou.go.jp/xmlpull.html""")
 st.write("""最新の情報は上から追加され下に押し出されます。（時系列なので同じ場所が違う時間で何度も掲載されています。）parser はXMLではなくAtom 向けの feedparser を使わないとデーターが取れません。""")
 st.divider()
 st.markdown("[go to Top](#section1)")
+
