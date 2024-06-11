ddf43881f1f6ebc7303043e2d411515a2a00dccb
kusanagi <65207089+kusanagi-tech@users.noreply.github.com>
2024-06-11 16:34:17 +0900

    reload button added

--- pages/currency.py
+++ pages/currency.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests as req
 import pandas as pd
+import datetime
 
 def currency():
   URL = """https://forex-api.coin.z.com/public/v1/ticker"""
@@ -12,7 +13,12 @@ df = pd.DataFrame(currency()).drop('status',axis=1)
 
 st.header("💰外為相場📈",anchor='section1',divider='rainbow')
 
+if st.button("**Reload**",type = "primary"):
+   st.experimental_rerun()
+
 st.table(df)
+st.write(datetime.datetime.now())  
 
 st.divider()
 st.markdown("[go to Top](#section1)")
+
